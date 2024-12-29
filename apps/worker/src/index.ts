3d8033510131957054544a5e902cafadf132a8cd
Nick Local <thenick112@gmail.com>
2024-12-29 14:46:28 +0000

    Add shadcn-svelte

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -5,29 +5,7 @@ import migrations from './../drizzle/migrations';
 import { Session, userTable } from "./db/user-schema";
 import { createSession, generateSessionToken } from "./auth/sessions";
 
-
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
-
-/** A Durable Object's behavior is defined in an exported Javascript class */
 export class CardioStore extends DurableObject {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-	 *
-	 * @param ctx - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 */
 	storage: DurableObjectStorage;
 	db: DrizzleSqliteDODatabase<any>;
 
@@ -48,23 +26,15 @@ export class CardioStore extends DurableObject {
 		return session;
 	}
 
-	/**
-	 * The Durable Object exposes an RPC method sayHello which will be invoked when when a Durable
-	 *  Object instance receives a request from a Worker via the same method invocation on the stub
-	 *
-	 * @param name - The name provided to a Durable Object instance from a Worker
-	 * @returns The greeting to be sent back to the Worker
-	 */
 	async sayHello(name: string): Promise<string> {
 		return `Hello, ${name}!`;
 	}
-	async insert(user: typeof users.$inferInsert) {
-        await this.db.insert(users).values(user);
+	async insert(user: typeof userTable.$inferInsert) {
+        await this.db.insert(userTable).values(user);
     }
 	async select() {
-        return this.db.select().from(users);
+        return this.db.select().from(userTable);
     }
-
 
 	async fetch(request: Request): Promise<Response> {
 		const url = new URL(request.url);
@@ -103,14 +73,6 @@ export class CardioStore extends DurableObject {
 }
 
 export default {
-	/**
-	 * This is the standard fetch handler for a Cloudflare Worker
-	 *
-	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 * @param ctx - The execution context of the Worker
-	 * @returns The response to be sent back to the client
-	 */
 	async fetch(request, env, ctx): Promise<Response> {
 		// We will create a `DurableObjectId` using the pathname from the Worker request
 		// This id refers to a unique instance of our 'MyDurableObject' class above
